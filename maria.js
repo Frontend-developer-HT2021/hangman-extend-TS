995d370e383dd03750f59eae350e57f25045cac5
Maria Kjellholm <maria.kjellholm@gmail.com>
2024-10-25 14:07:11 +0200

    funktion för randomiserat ord

--- maria.js
+++ maria.js
@@ -1,1 +1,12 @@
-hej
+let wordList = ["hej", "de", "lorem", "ipsum", "blä", "kom", "då", "lilla", "skit", "korv"];
+getRandomWord(wordList);
+
+
+function getRandomWord(wordList) {
+    let randomWord = Math.floor(Math.random() * wordList.length);
+    return wordList.splice(randomWord, 1)[0];    
+}
+
+console.log(getRandomWord(wordList)); 
+console.log(wordList); 
+
